8451ef5f848b88ec703ba466feae28992aed63e4
j1_201611105 정수환 <jshking7177@naver.com>
2016-09-21 01:57:03 +0900

    9.19 과제

---
+++ w2main_1.java
@@ -0,0 +1,14 @@
+﻿jshell> class Hello {
+   ...>    String toWhom="java";
+   ...>    Hello() {}
+   ...>    void sayHello() {
+   ...>       System.out.println("hello "+toWhom);
+   ...>    }
+   ...> }
+|  created class Hello
+
+jshell> Hello h=new Hello();
+h ==> Hello@239963d8
+
+jshell> h.sayHello();
+hello java
---
+++ w2main_2.java
@@ -0,0 +1,32 @@
+﻿jshell> class Subway {
+   ...>   int line;
+   ...>   void run() { System.out.println("4lines runs");
+   ...>   }
+   ...> }
+|  created class Subway
+
+jshell> Subway mySub=new Subway();
+mySub ==> Subway@31610302
+
+jshell> mySub.run()
+4lines runs
+
+jshell> class Subway {
+   ...>   int line;
+   ...>   void run() { System.out.println (line + "lines runs");
+   ...>   }
+   ...>   void setLine(int l) {
+   ...>     line=l;
+   ...>   }
+   ...> }
+|  replaced class Subway
+|    update replaced variable mySub, reset to null
+
+jshell> Subway mySub=new Subway();
+mySub ==> Subway@52a86356
+
+jshell> mySub.setLine(1);
+            ^
+
+jshell> mySub.run();
+1lines runs
