84b50f7b52959d8b6ca8b62c51d4d650bef7a907
j1_201611105 정수환 <jshking7177@naver.com>
2016-09-26 00:00:11 +0900

    9.22 과제

---
+++ w3main_2.java
@@ -0,0 +1,21 @@
+﻿class BrazilCoffee {
+   String origin;
+   int roast;
+   int grade;
+   BrazilCoffee(String o) {
+      origin=o;
+   }
+   public void roast() {
+      System.out.println("Roasting from "+origin);
+   }
+   public void grind() {
+      System.out.println("Grinding from "+origin);
+   }
+   public void brew() {
+      System.out.println("Brewing from "+origin);
+   }
+}
+BrazilCoffee a=new BrazilCoffee("Brazil");
+a.roast();
+a.brew();
+a.grind();
