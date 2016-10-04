9577552b3a8b61663c7070dc6461004b37d8359e
j1_201611105 정수환 <jshking7177@naver.com>
2016-10-05 02:11:11 +0900

    9.28 과제

---
+++ w3main_3.java
@@ -0,0 +1,47 @@
+﻿ public class Rectangle {
+   ...>    private int width;
+   ...>    private int height;
+   ...> public Rectangle (int width, int height) {
+   ...>    this.width=width;
+   ...>    this.height=height;
+   ...> }
+   ...> public int getWidth() {
+   ...>    return width;
+   ...> }
+   ...> public int getHeight() {
+   ...>    return height;
+   ...> }
+   ...> public void setWidth(int newWidth) {
+   ...> width=newWidth;
+   ...> }
+   ...> public void setHeight(int newHeight) {
+   ...> height=newHeight;
+   ...> }
+   ...> public void setSize(int width, int height) {
+   ...> setWidth(width);
+   ...> setHeight(height);
+   ...> }
+   ...> public int calcArea() {
+   ...>
+   ...>    return width*height;
+   ...> }
+   ...> }
+|  Warning:
+|  Modifier 'public'  not permitted in top-level declarations, ignored
+|  public class Rectangle {
+|  ^----^
+|  created class Rectangle this error is corrected:
+|      Modifier 'public'  not permitted in top-level declarations, ignored
+|      public class Rectangle {
+|      ^----^
+
+jshell> Rectangle q=new Rectangle(6,6);
+q ==> Rectangle@239963d8
+
+jshell> q.calcArea();
+$3 ==> 36
+
+jshell> q.setSize(3,3);
+
+jshell> q.calcArea();
+$5 ==> 9
