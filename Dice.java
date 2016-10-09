f9c8f70f8a4d58a52f2518fdbf092b753b68982d
j1_201611105 정수환 <jshking7177@naver.com>
2016-10-10 02:43:31 +0900

    10.5 과제

---
+++ Dice.java
@@ -0,0 +1,8 @@
+﻿class Dice{
+	private int number=0;
+	public int roll() {
+		this.number = (int) (Math.random()*6)+1;
+		System.out.println("turns on " +number);
+		return number;
+	}
+}
---
+++ DiceGame.java
@@ -0,0 +1,21 @@
+﻿class DiceGame {
+private Player p1;
+private Player p2;
+public void startGame() {
+p1=new Player("A");
+p2=new Player("B");
+p1.play();
+p2.play();
+if(p1.getNum() > p2.getNum()) {
+System.out.println(p1.getName()+" win");
+} else if (p1.getNum() == p2.getNum()) {
+System.out.println("draw");
+}
+else {
+System.out.println(p2.getName() + " win");
+}
+}
+}
+
+DiceGame d=new DiceGame();
+d.startGame();
