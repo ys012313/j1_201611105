12b51f10a8ab60c2cc68e3d6c25ae17997fefccc
j1_201611105 정수환 <jshking7177@naver.com>
2016-10-10 03:13:21 +0900

    10.5 과제

---
+++ Player.java
@@ -0,0 +1,19 @@
+﻿public class Player {
+    private String name=null;
+    int num=0;
+    public Player(String _name) {
+        this.name=_name;
+    }
+    public void play() {
+        Dice d=new Dice();
+        //num=d.roll();
+        d.roll();
+        num=d.getNum();
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getNum() {
+        return this.num;
+    }
+}
