d3dcafcf3ba2f71447e556d9ab501b34ffceafb6
j1_201611105 정수환 <jshking7177@naver.com>
2016-10-17 06:43:50 +0900

    HelloWorld2.java
    HelloWorld2.java/pakage

---
+++ HelloWorld2.java
@@ -0,0 +1,13 @@
+package com.sd.hello;
+
+class HelloWorld2 {
+  public void hello() {
+    System.out.println("Hello World from hello()");
+  }
+  public void hello(String name) {
+    System.out.printf("Hello World %s", name);
+  }
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
