69944fb281ffa571eb9b745fa36b2668d6400559
j1_201611105 정수환 <jshking7177@naver.com>
2016-10-17 07:14:55 +0900

    TurtleDemoMain.java / TurtleDemoMain.png

---
+++ TurtleDemoMain.java
@@ -0,0 +1,20 @@
+package com.sd.turtle;
+import ch.aplu.turtle.*;
+
+class TurtleDemoMain{
+         Turtle t1=new Turtle();
+         TurtleDemoMain(){
+               t1.forward(77);
+               t1.left(90);
+               t1.forward(77);
+               t1.left(90);
+               t1.forward(77);
+               t1.left(90);
+               t1.forward(77);
+               t1.left(90);
+               }
+         
+         public static void main(String[] args){
+               new TurtleDemoMain();
+         }
+}
