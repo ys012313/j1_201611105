547ce4240778b18f95d2e9ac789d74a48df74f98
j1_201611105 정수환 <jshking7177@naver.com>
2016-12-21 21:28:45 +0900

    Update CalcAreaMain.java

--- CalcAreaMain.java
+++ CalcAreaMain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CalcAreaMain {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("radius please");
+    System.out.print("input radius");
     double r = sc.nextDouble();
     double area = Math.PI*Math.pow(r,2);
     System.out.printf("radius:%.2f area:%.2f",r,area);
