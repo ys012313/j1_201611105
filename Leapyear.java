170125b6980fde7e32ec32a24bb4ba09fdb43fab
j1_201611105 정수환 <jshking7177@naver.com>
2016-12-21 18:56:16 +0900

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,18 @@
+class Leapyear{
+	int year;
+	void findyear(int year){
+		if ((year%4==0) && (year%100!=0)||(year%400==0)) {
+			System.out.printf("%d a leap year \n",year);
+		}
+		else {
+			System.out.printf("%d not a leap year \n",year);
+		}
+	}
+	public static void main(String[] args) {
+		Leapyear e = new Leapyear();
+		int[] years = {1800,1900,1912,1984,1985,2000,2015,1825,1928,2031,1845,1947,2053,2099};
+		for(int y:years){
+			e.findyear(y);
+		}
+	}
+}
